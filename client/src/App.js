7ab3367a75b6d18a716d8062719d63c1e7b99f6b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-11 09:42:56 +0530

    cs

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ChatBox from './components/ChatBox';
 
 function App() {
   return (
     <div className="App">
-      <h1>Customer Support Chatbot</h1>
-      <ChatBox />
+      <header>
+        <h1>Customer Support Chatbot</h1>
+      </header>
+      <main>
+        <ChatBox />
+      </main>
     </div>
   );
 }
