4620ff801cc65d9492ac28947070b3341206b62c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-11 11:14:57 +0530

    cs

---
+++ server/models/Chat.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+  sender: String,
+  text: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Chat = mongoose.model('Chat', chatSchema);
+module.exports = Chat;
