832530d7e9202b5e77d5b936cc26351ad1300d45
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-13 10:44:06 +0530

    cs

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true, unique: true, trim: true, lowercase: true },
+  password: { type: String, required: true, minlength: 8 },
 });
 
 userSchema.pre('save', async function (next) {
@@ -12,6 +12,10 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
