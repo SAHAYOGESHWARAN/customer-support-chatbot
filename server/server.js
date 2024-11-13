390219ffb49e98e46a7e74c0665b8dabf4374d5c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-13 11:02:05 +0530

    cs

--- server/server.js
+++ server/server.js
@@ -17,10 +17,11 @@ app.use('/api/chatbot', chatbotRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
   .catch(err => {
-    console.log(err);
+    console.error(err);
     process.exit(1);
   });
-  
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
