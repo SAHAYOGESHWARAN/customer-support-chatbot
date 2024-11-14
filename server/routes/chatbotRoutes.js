0d217182f3bc2ed823e27dd80f79a43508bcc065
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-14 08:50:52 +0530

    cs

--- server/routes/chatbotRoutes.js
+++ server/routes/chatbotRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { processMessage } = require('../controllers/chatbotController');
 
-/**
- * Route to handle incoming chatbot messages.
- * @route POST /message
- */
+
 router.post('/message', processMessage);
 
 module.exports = router;
